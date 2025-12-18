e48d3ef7c9fe9840159cb1e177464257bde4ab40
Prasanna kumar reddy <mrpkreddy94@gmail.com>
2025-12-18 09:41:30 +0530

    trying second sample script

---
+++ 02.variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Pk - Hi ,how are you"
+echo "bablu - wtsup? how was your preparation"
+echo "Pk - its good i need to push more to become pro"
+echp "bablu - ok put more effortd you can"
