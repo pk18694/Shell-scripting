d5d57c16786b6ecd47cfe15e3744839d74b12837
Prasanna kumar reddy <mrpkreddy94@gmail.com>
2025-12-18 11:40:33 +0530

    trying new features or arguments and user n paswd

--- 03.variables.sh
+++ 03.variables.sh
@@ -5,6 +5,6 @@ PERSON2=$2
 PERSON3=$3
 
 echo "$PERSON1 : wtsup ?what are doing"
-echo "$PERSON2 :nothing much waiting for you PERSON3."
+echo "$PERSON2 :nothing much waiting for you $PERSON3."
 echo "$PERSON3 :so where we will go "
 
