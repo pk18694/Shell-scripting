4788c931c056c878c4401ce8ffc9f20b51f73b6a
Prasanna kumar reddy <mrpkreddy94@gmail.com>
2025-12-18 09:43:39 +0530

    modified second script

--- 02.variables.sh
+++ 02.variables.sh
@@ -3,4 +3,4 @@
 echo "Pk - Hi ,how are you"
 echo "bablu - wtsup? how was your preparation"
 echo "Pk - its good i need to push more to become pro"
-echp "bablu - ok put more effortd you can"
+echo "bablu - ok put more effortd you can"
